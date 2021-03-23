886898eb21ba174f5643c02c9a6de75a6b83283a
RGJ <rgj@celum.com>
2021-03-23 13:25:14 +0100

    adaptions to trigger pipelines

--- Pipelines/trigger_pipeline.sh
+++ Pipelines/trigger_pipeline.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-location="https://dev.azure.com/<organization>/<project>/_apis/build/builds?api-version=6.0"
+# $1 is the sourceBranch of the project, the same branch of the pipeline will be triggered on another pipeline
+# $2 is the system-access-token which is used for azure authentication
+# $3 is the ID of the pipeline to trigger
+
+location="https://dev.azure.com/<org>/<project>/_apis/build/builds?api-version=6.0"
 
 data="{
     \"definition\": {
-        \"id\": <number>
+        \"id\": $3
     },
     \"sourceBranch\": \"$1\"
 }"
 
 echo 'below is the sourceBranch'
 echo $1
+echo 'below is the '
+echo $3
+echo $data
 
 response=$(curl -H "Authorization: Bearer $2" --request POST $location --header "Content-Type: application/json" -d "$data")
 echo $response
